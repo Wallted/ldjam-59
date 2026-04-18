165a129829e886b7aa047c3720b7eb1f4805127f
TheLastSlowpoke <39202881+TheLastSlowpoke@users.noreply.github.com>
2026-04-18 17:08:38 +0200

    synth api (#5)

---
+++ chorister.gd
@@ -0,0 +1,26 @@
+class_name Chorister
+extends RefCounted
+
+
+var _freq: float
+var _phase := 0.0
+var _increment := 0.0
+
+
+func _init(freq: float) -> void:
+	change_freq(freq)
+
+
+func synthesize():
+	var frame = _specific_math()
+	_phase = fmod(_phase + _increment, 1.0)
+	return frame
+
+
+func change_freq(new_freq: float):
+	_freq = new_freq
+	_increment = _freq / Audio.config_sample_hz
+
+
+func _specific_math():
+	return 0.0
--- game.gd
+++ game.gd
@@ -1,1 +1,12 @@
 extends Control
+
+
+@onready var audio: Audio = $Audio
+
+
+func _ready() -> void:
+	# mock
+	audio.target_choir.append(Sinoceros.new(440.0))
+	audio.target_choir.append(Sinoceros.new(640.0))
+	audio.target_choir.append(Sinoceros.new(840.0))
+	audio.play_target_choir = true

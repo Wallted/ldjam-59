7da83a97e196b22bb2ebff99f1405918f40db2d2
Grzegorz Choinski <grzegorzchoinski97@gmail.com>
2026-04-19 01:18:11 +0200

    some cleanup for animal and zoo (#11)
    Signed-off-by: Grzegorz Choiński <grzegorzchoinski97@gmail.com>

--- animal.gd
+++ animal.gd
@@ -1,45 +1,45 @@
 class_name Animal
 extends Node2D
 
-@onready var button = $TextureButton
-@onready var label = $Label
 @export var zoo_space: ZooSpace
 
 const const_lerp_catch_factor_velocity = 1.5
 
-var pressing = false
-var lerp_factor = 0.0
-var previous_point: Vector2
+var _is_pressing = false
+var _lerp_factor = 0.0
+var _previous_point: Vector2
 
 func _process(delta: float) -> void:
-	if pressing:
-		var global_coords = get_global_mouse_position()
-		lerp_factor += clamp(const_lerp_catch_factor_velocity * delta, 0, 1)
-		global_position = lerp(global_position, global_coords, ease(lerp_factor, -4.0))
-	else:
-		lerp_factor = 0.0
+	handle_mouse_drag(delta)
 
-	if pressing:
-		zoo_space.grid_process_cell(global_position)
-	else:
-		if not zoo_space.grid_is_in_bounds(global_position) and previous_point:
-			global_position = lerp(global_position, previous_point, 0.1)
-		else:
-			previous_point = global_position
-			#print('previous point %s' % previous_point)
-	
-	if zoo_space.grid_is_in_bounds(global_position):
-		previous_point = global_position
+func handle_mouse_drag(delta: float):
+	if _is_pressing:
+		var global_coords = get_global_mouse_position()
+		_lerp_factor += clamp(const_lerp_catch_factor_velocity * delta, 0, 1)
+		global_position = lerp(global_position, global_coords, ease(_lerp_factor, -4.0))
+		zoo_space.grid_process_cell(global_position) # debug
+
+	var is_in_bounds = zoo_space.grid_is_in_bounds(global_position)
+
+	if not _is_pressing:
+		if not is_in_bounds and _previous_point:
+			global_position = lerp(global_position, _previous_point, 0.1)
+
+	if is_in_bounds:
+		_previous_point = global_position
 		var local_position_normalized = zoo_space.grid_get_local_position_normalized(global_position)
 		update_label('%s' % local_position_normalized)
 	else:
 		update_label('OOO')
-	
+
 func _on_texture_button_button_up() -> void:
-	pressing = false
+	_is_pressing = false
+	_lerp_factor = 0.0
 
 func _on_texture_button_button_down() -> void:
-	pressing = true
+	_is_pressing = true
 
+# trash
+@onready var label = $Label
 func update_label(text):
 	label.text = text

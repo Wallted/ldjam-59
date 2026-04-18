c5f9d53eb94ec09b607107069b07c9b62940b35f
Grzegorz Choiński <grzegorzchoinski97@gmail.com>
2026-04-19 01:03:30 +0200

    some cleanup for animal and zoo
    Signed-off-by: Grzegorz Choiński <grzegorzchoinski97@gmail.com>

--- zoo_space.gd
+++ zoo_space.gd
@@ -1,47 +1,41 @@
 class_name ZooSpace
 extends Control
-@onready var tilemap = $TileMapLayer
-const ANIMAL = preload("res://animal.tscn")
-var rows_x = 4
-var rows_y = 4
 
-var scaled_size: Vector2
-var _scale: float
+const ANIMAL = preload("res://animal.tscn")
+
+@onready var tilemap = $TileMapLayer
+
+@export var rows_x = 4
+@export var rows_y = 4
+
+var grid_size_scaled: Vector2
 
 func _ready() -> void:
 	grid_generate()
-	setup_animals([
-		ANIMAL.instantiate(), ANIMAL.instantiate()
-	])
+	animals_setup([ANIMAL.instantiate(), ANIMAL.instantiate()])
 
-func setup_animals(animals: Array[Animal]):
+func animals_setup(animals: Array[Animal]):
 	for animal in animals:
 		animal.zoo_space = self
-		animal.position = Vector2(randi_range(0, scaled_size.x), randi_range(0, scaled_size.y))
+		animal.position = Vector2(randi_range(0, int(grid_size_scaled.x)), randi_range(0, int(grid_size_scaled.y)))
 		add_child(animal)
 
 func grid_generate():
-	tilemap.clear()
-	
 	var tile_size = tilemap.tile_set.tile_size
-	
 	var wanted_size_x = tile_size.x * rows_x
 	var wanted_size_y = tile_size.y * rows_y
-	var size_x = size.x
-	var size_y = size.y
-	
-	_scale = minf(size_x / wanted_size_x, size_y / wanted_size_y)
+	var wanted_scale = minf(size.x / wanted_size_x, size.y / wanted_size_y)
+	grid_size_scaled = Vector2(wanted_size_x, wanted_size_y) * wanted_scale
 	
 	var offset_x = 0
 	var offset_y = 0
-	if size_x > (wanted_size_x * _scale):
-		offset_x = (size_x - (wanted_size_x * _scale))/2
-	if size_y > (wanted_size_y * _scale):
-		offset_y = (size_y - (wanted_size_y * _scale))/2
+	if size.x > (wanted_size_x * wanted_scale):
+		offset_x = (size.x - (wanted_size_x * wanted_scale))/2
+	if size.y > (wanted_size_y * wanted_scale):
+		offset_y = (size.y - (wanted_size_y * wanted_scale))/2
 	
-	scaled_size = Vector2(wanted_size_x, wanted_size_y) * _scale
-	
-	tilemap.scale = Vector2(_scale, _scale)
+	tilemap.clear()
+	tilemap.scale = Vector2(wanted_scale, wanted_scale)
 	tilemap.position = Vector2(offset_x, offset_y)
 	for x in rows_x:
 		for y in rows_y:
@@ -49,23 +43,23 @@ func grid_generate():
 
 func grid_get_local_position(global_coords: Vector2):
 	""" Posision relative to TileMap """
-
 	var local_postion = global_position - global_coords + tilemap.position # why this works with global ?
 	return -local_postion # why this needs to be negated ?
 
 func grid_get_local_position_normalized(global_coords: Vector2) -> Vector2:
 	""" Normalize each cell in grid to be between (0, 1)"""
-
 	var local_postion = grid_get_local_position(global_coords)
-	return local_postion / (Vector2(tilemap.tile_set.tile_size) * _scale)
-
-func grid_process_cell(global_coords: Vector2):
-	var local_postion = grid_get_local_position(global_coords)
-	var cell_coords = tilemap.local_to_map(local_postion/tilemap.scale)
-	tilemap.erase_cell(cell_coords)
+	return local_postion / (Vector2(tilemap.tile_set.tile_size) * tilemap.scale)
 
 func grid_is_in_bounds(global_coords: Vector2):
 	var local_position = grid_get_local_position(global_coords)
-	
-	var rect = Rect2(Vector2.ZERO, Vector2.ZERO).expand(scaled_size)
+	var rect = Rect2(Vector2.ZERO, Vector2.ZERO).expand(grid_size_scaled)
 	return rect.has_point(local_position) or global_coords == local_position
+	
+func grid_get_cell_coords(global_coords: Vector2) -> Vector2i:
+	var local_postion = grid_get_local_position(global_coords)
+	return tilemap.local_to_map(local_postion/tilemap.scale)
+	
+# trash
+func grid_process_cell(global_coords: Vector2):
+	tilemap.erase_cell(grid_get_cell_coords(global_coords))

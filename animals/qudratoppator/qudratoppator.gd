871e74d9e865d69f14b8fed372af64fac401a8eb
tyminix <86685958+tyminix@users.noreply.github.com>
2026-04-19 19:19:35 +0200

    rebranding and adding animals

--- animals/qudratoppator/qudratoppator.gd
+++ animals/qudratoppator/qudratoppator.gd
@@ -1,4 +1,5 @@
 class_name qudratoppator
+class_name Qudratoppator
 extends Chorister
 
 
--- species.gd
+++ species.gd
@@ -1,13 +1,30 @@
 extends Node
 
 enum ChoristerEnum {
+	CHORIPRACTER,
 	SINOCEROS,
+	QUDRATOPPATOR,
+	OGGAZELE,
+	DELAILER,
+	TIGEREVERBER
 }
 
+	
 var ChoristerMap = {
+	ChoristerEnum.CHORIPRACTER: Choripracter,
 	ChoristerEnum.SINOCEROS: Sinoceros,
+	ChoristerEnum.QUDRATOPPATOR: Qudratoppator,
+	ChoristerEnum.OGGAZELE: Oggazele,
+	ChoristerEnum.DELAILER: Delailer,
+	ChoristerEnum.TIGEREVERBER: Tigereverber
 }
 
 var ChoristerResMap = {
 	ChoristerEnum.SINOCEROS: preload("res://icon.svg")
+	ChoristerEnum.CHORIPRACTER:  preload("res://icon.svg"),
+	ChoristerEnum.SINOCEROS:  preload("res://icon.svg"),
+	ChoristerEnum.QUDRATOPPATOR:  preload("res://icon.svg"),
+	ChoristerEnum.OGGAZELE:  preload("res://icon.svg"),
+	ChoristerEnum.DELAILER:  preload("res://icon.svg"),
+	ChoristerEnum.TIGEREVERBER:  preload("res://icon.svg")
 }

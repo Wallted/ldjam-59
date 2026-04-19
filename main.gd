6eca703b1f34fb2f638ab3e81df61db96bc75619
Grzegorz Choinski <grzegorzchoinski97@gmail.com>
2026-04-19 14:14:18 +0200

    more game/level refactor (#20)
    Signed-off-by: Grzegorz Choiński <grzegorzchoinski97@gmail.com>

--- main.gd
+++ main.gd
@@ -3,4 +3,30 @@ extends Node
 @onready var game: Game = $Game
 
 func _ready() -> void:
-	game.load_new_level()
+	game.load_new_level(get_level_data(0))
+
+func get_level_data(_idx): 
+	var level_data = { # mock
+		"x": 30,
+		"y": 20,
+		"player_data": [
+			# species, freq
+			[0, 200.0],
+			[0, 200.0],
+		], 
+		"target_data": [
+			[0, 200.0],
+			[0, 200.0],
+			[0, 200.0],
+			[0, 200.0],
+			[0, 200.0],
+			[0, 200.0],
+			[0, 200.0],
+			[0, 200.0],
+			[0, 200.0],
+			[0, 200.0],
+			[0, 200.0],
+			[0, 200.0],
+		],
+	}
+	return level_data

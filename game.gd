875ab1206b3e59ec37d501760a812de76ada915c
adasium <adasium@proton.me>
2026-04-19 20:43:52 +0200

    Add back button (#35)

--- game.gd
+++ game.gd
@@ -3,8 +3,13 @@ extends Control
 
 @onready var audio: Audio = $Audio
 @onready var zoo_space: ZooSpace = $ZooSpace
+@onready var back_button: BackButton = $BackButton
 
 var level: Level
+signal go_to_menu()
+
+func _ready():
+	back_button.pressed.connect(func(): go_to_menu.emit())
 
 func load_new_level(level_data: LevelData) -> void:
 	level = Level.new(level_data)

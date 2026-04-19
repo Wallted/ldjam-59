4eeaa36b7dd07c91c78558195d09dee69cb91455
tyminix <86685958+tyminix@users.noreply.github.com>
2026-04-19 15:06:17 +0200

    asdasdasdasd

--- audio.gd
+++ audio.gd
@@ -23,7 +23,7 @@ var audio_stream_player_table: Array[AudioStreamPlayer] = []
 
 func _ready() -> void:
 	_audio_stream_player.stream.mix_rate = config_sample_hz
-	audio_stream_player_table = [$AudioStreamPlayer/AudioStreamPlayer1,$AudioStreamPlayer/AudioStreamPlayer2,$AudioStreamPlayer/AudioStreamPlayer3,$AudioStreamPlayer/AudioStreamPlayer4,$AudioStreamPlayer/AudioStreamPlayer5,$AudioStreamPlayer/AudioStreamPlayer6,$AudioStreamPlayer/AudioStreamPlayer7,$AudioStreamPlayer/AudioStreamPlayer8,$AudioStreamPlayer/AudioStreamPlayer9,$AudioStreamPlayer/AudioStreamPlayer10,$AudioStreamPlayer/AudioStreamPlayer11,$AudioStreamPlayer/AudioStreamPlayer12]
+	audio_stream_player_table = [$AudioStreamPlayer/AudioStreamPlayer1,$AudioStreamPlayer/AudioStreamPlayer2,$AudioStreamPlayer/AudioStreamPlayer3,$AudioStreamPlayer/AudioStreamPlayer4,$AudioStreamPlayer/AudioStreamPlayer5]
 
 func _process(_delta: float) -> void:
 	match who_sings:
@@ -37,7 +37,6 @@ func _fill_buffer(choir: Array[Chorister]) -> void:
 	
 	assert(choir.size() <= 12)
 	for i in choir.size():
-		print(i)
 		var playback = audio_stream_player_table[i].get_stream_playback()
 		var to_fill: int = playback.get_frames_available()
 		while to_fill > 0:
--- game.gd
+++ game.gd
@@ -7,15 +7,8 @@ extends Control
 func _ready() -> void:
 	# mock
 	audio.target_choir.append(Sinoceros.new(200.0))
-	audio.target_choir.append(Sinoceros.new(200.0))
-	audio.target_choir.append(Sinoceros.new(200.0))
-	audio.target_choir.append(Sinoceros.new(200.0))
-	audio.target_choir.append(Sinoceros.new(200.0))
-	audio.target_choir.append(Sinoceros.new(200.0))
-	audio.target_choir.append(Sinoceros.new(200.0))
-	audio.target_choir.append(Sinoceros.new(200.0))
-	audio.target_choir.append(Sinoceros.new(200.0))
-	audio.target_choir.append(Sinoceros.new(200.0))
-	audio.target_choir.append(Sinoceros.new(200.0))
-	audio.target_choir.append(Sinoceros.new(200.0))
+	audio.target_choir.append(Sinoceros.new(110.0))
+	audio.target_choir.append(qudratoppator.new(220.0))
+	audio.target_choir.append(Sinoceros.new(90.0))
+	
 	audio.who_sings = Audio.WhoSings.Target

221da1962507ffe29459bf9d03cfd42f54a0b5e7
TheLastSlowpoke <39202881+TheLastSlowpoke@users.noreply.github.com>
2026-04-19 15:36:05 +0200

    play buttons working, edge cases still waiting to be smoothed out (#23)

--- audio.gd
+++ audio.gd
@@ -13,8 +13,6 @@ enum WhoSings{
 
 var who_sings := WhoSings.None
 
-var player_choir: Array[Chorister] = []
-var target_choir: Array[Chorister] = []
 var audio_stream_player_table: Array[AudioStreamPlayer] = []
 
 @onready var _audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
@@ -23,19 +21,19 @@ var level: Level
 
 func _ready() -> void:
 	_audio_stream_player.stream.mix_rate = config_sample_hz
-	audio_stream_player_table = [$AudioStreamPlayer/AudioStreamPlayer1,$AudioStreamPlayer/AudioStreamPlayer2,$AudioStreamPlayer/AudioStreamPlayer3,$AudioStreamPlayer/AudioStreamPlayer4,$AudioStreamPlayer/AudioStreamPlayer5,$AudioStreamPlayer/AudioStreamPlayer6,$AudioStreamPlayer/AudioStreamPlayer7,$AudioStreamPlayer/AudioStreamPlayer8,$AudioStreamPlayer/AudioStreamPlayer9,$AudioStreamPlayer/AudioStreamPlayer10,$AudioStreamPlayer/AudioStreamPlayer11,$AudioStreamPlayer/AudioStreamPlayer12]
+	audio_stream_player_table = [$AudioStreamPlayer/AudioStreamPlayer1,$AudioStreamPlayer/AudioStreamPlayer2,$AudioStreamPlayer/AudioStreamPlayer3,$AudioStreamPlayer/AudioStreamPlayer4,$AudioStreamPlayer/AudioStreamPlayer5]
 
 func _process(_delta: float) -> void:
 	match who_sings:
 		WhoSings.Target:
-			_fill_buffer(target_choir)
+			_fill_buffer(level.target_choir)
 		WhoSings.Player:
-			_fill_buffer(player_choir)
+			_fill_buffer(level.player_choir)
 
 
 func _fill_buffer(choir: Array[Chorister]) -> void:
 	
-	assert(choir.size() <= 12)
+	assert(choir.size() <= 5)
 	for i in choir.size():
 		var playback = audio_stream_player_table[i].get_stream_playback()
 		var to_fill: int = playback.get_frames_available()

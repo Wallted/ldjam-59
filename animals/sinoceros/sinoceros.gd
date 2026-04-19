a59e34cef78d5954c835ce0f91e70c447d7f2bcd
tyminix <86685958+tyminix@users.noreply.github.com>
2026-04-20 00:00:11 +0200

    position y effect sound (#40)

--- animals/sinoceros/sinoceros.gd
+++ animals/sinoceros/sinoceros.gd
@@ -3,4 +3,5 @@ extends Chorister
 
 
 func _specific_math():
+	AudioServer.get_bus_effect(AudioServer.get_bus_index("Sinoceros"),1).set_rate_hz((self.grid_position.y/self.grid_size.y*10+1))
 	return sin(_phase * TAU)

4cba03427a22423c8b36d431b5c60ee3b7eda0af
Jan Czubiak <janczubiak@wp.pl>
2026-04-19 17:04:20 +0200

    fractal frames

--- canvas.gd
+++ canvas.gd
@@ -1,2 +1,13 @@
 class_name Fractal
 extends Control
+
+
+signal interaction(is_pressed: bool)
+
+
+func _on_button_button_down():
+	interaction.emit(true)
+
+
+func _on_button_button_up():
+	interaction.emit(false)

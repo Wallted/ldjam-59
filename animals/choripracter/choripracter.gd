10e0c68c92606ed5c0db1a30a7ebf94ed08728ba
tyminix <86685958+tyminix@users.noreply.github.com>
2026-04-21 00:21:25 +0200

    super voicing

--- animals/choripracter/choripracter.gd
+++ animals/choripracter/choripracter.gd
@@ -5,4 +5,8 @@ extends Chorister
 func _specific_math():
 	#AudioServer.get_bus_effect(AudioServer.get_bus_index("Choripracter"),1).set_wet((self.grid_position.y/self.grid_size.y/1.5+0.1))
 	AudioServer.get_bus_effect(AudioServer.get_bus_index("Choripracter"),1).set_wet(((EFFECT_MAX_GRID_SIZE - self.grid_position.y)/EFFECT_MAX_GRID_SIZE/1.5+0.1))
-	return sin(_phase * TAU)-cos(_phase * TAU*2) 
+	AudioServer.get_bus_effect(AudioServer.get_bus_index("Choripracter"),2).set_wet((((EFFECT_MAX_GRID_SIZE - self.grid_position.y))/EFFECT_MAX_GRID_SIZE))
+	AudioServer.get_bus_effect(AudioServer.get_bus_index("Choripracter"),3).set_drive(((EFFECT_MAX_GRID_SIZE - self.grid_position.y)/EFFECT_MAX_GRID_SIZE/3))
+
+	return sin(_phase * TAU*1.2)+sin(_phase * TAU*2)
+	#return sin(_phase * TAU)-cos(_phase * TAU*2) 

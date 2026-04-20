6cc88faaa74ace9197e6f1e4b884ac14c80ef3d7
Grzegorz Choiński <grzegorzchoinski97@gmail.com>
2026-04-20 02:10:51 +0200

    titles
    add background tiles and indicate zoo space
    
    Signed-off-by: Grzegorz Choiński <grzegorzchoinski97@gmail.com>

--- game.gd
+++ game.gd
@@ -6,9 +6,12 @@ extends Control
 @onready var back_button: BackButton = $BackButton
 @onready var current_fractal: Fractal = $CurrentFractal
 @onready var target_fractal: Fractal = $TargetFractal
+@onready var background = $Background
 
 var level: Level
 signal go_to_menu()
+
+var rng = RandomNumberGenerator.new()
 
 func _ready():
 	back_button.pressed.connect(_exit_level)
@@ -22,12 +25,14 @@ func load_new_level(level_data: LevelData) -> void:
 	target_fractal.start(level)
 	zoo_space.restart()
 
+	for x in 8:
+		for y in 5:
+			background.set_cell(Vector2(x, y), rng.randi_range(0, 3), Vector2i(0, 0))
 
 func _exit_level():
 	current_fractal.stop()
 	target_fractal.stop()
 	go_to_menu.emit()
-
 
 func _on_play_button(is_pressed: bool, is_player: bool):
 	audio.who_sings = Audio.WhoSings.None
--- zoo_space.gd
+++ zoo_space.gd
@@ -4,7 +4,7 @@ extends Control
 const GRID_ANIMAL = preload("res://grid_animal.tscn")
 
 @onready var tilemap = $TileMapLayer
-
+@onready var texture_rect = $TextureRect
 var grid_size_scaled: Vector2
 
 var level: Level
@@ -34,6 +34,7 @@ func spawn_animals(choristers: Array[Chorister]):
 
 func grid_generate(dimension_x, dimension_y):
 	var tile_size = tilemap.tile_set.tile_size
+	print('tile_size', tile_size)
 	var wanted_size_x = tile_size.x * dimension_x
 	var wanted_size_y = tile_size.y * dimension_y
 	var wanted_scale = minf(size.x / wanted_size_x, size.y / wanted_size_y)
@@ -48,9 +49,14 @@ func grid_generate(dimension_x, dimension_y):
 
 	tilemap.scale = Vector2(wanted_scale, wanted_scale)
 	tilemap.position = Vector2(offset_x, offset_y)
+	var rng = RandomNumberGenerator.new()
 	for x in dimension_x:
 		for y in dimension_y:
-			tilemap.set_cell(Vector2(x, y), 0, Vector2i(0, 0))
+			tilemap.set_cell(Vector2(x, y), rng.randi_range(0, 1), Vector2i(0, 0))
+
+	texture_rect.size = grid_size_scaled * 1.1
+	texture_rect.position = tilemap.position - (texture_rect.size/2.0 - grid_size_scaled/2.0)
+	texture_rect.set_instance_shader_parameter('aspect_ratio', float(level.x)/float(level.y))
 
 func grid_get_local_position(global_coords: Vector2):
 	""" Posision relative to TileMap """

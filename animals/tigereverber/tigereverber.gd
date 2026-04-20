07f4192540b703ebe1a26bcef1c86b9c71d15a93
TheLastSlowpoke <39202881+TheLastSlowpoke@users.noreply.github.com>
2026-04-20 13:39:30 +0200

    Tic frac toe (#49)
    * audio effect Y flipped and fixed to the grid
    
    * fractals bijected info effects
    
    * 5 fractal species done i guess
    
    * mehrghe

--- animals/tigereverber/tigereverber.gd
+++ animals/tigereverber/tigereverber.gd
@@ -3,5 +3,6 @@ extends Chorister
 
 
 func _specific_math():
-	AudioServer.get_bus_effect(AudioServer.get_bus_index("Tigereverber"),2).set_wet((self.grid_position.y/self.grid_size.y))
+	#AudioServer.get_bus_effect(AudioServer.get_bus_index("Tigereverber"),2).set_wet((self.grid_position.y/self.grid_size.y))
+	AudioServer.get_bus_effect(AudioServer.get_bus_index("Tigereverber"),2).set_wet(((EFFECT_MAX_GRID_SIZE - self.grid_position.y)/EFFECT_MAX_GRID_SIZE))
 	return sin(_phase * TAU*1.2)+sin(_phase * TAU*2)

3a7bbe76037143310fe055c488a446078c532e4d
tyminix <86685958+tyminix@users.noreply.github.com>
2026-04-21 00:30:15 +0200

    super voicing (#77)

--- animals/delailer/delailer.gd
+++ animals/delailer/delailer.gd
@@ -1,8 +1,9 @@
 class_name Delailer
 extends Chorister 
-var i: float
-var a:=1.0
-
+var i: int
+var a:=0.0
+var _aprev:=0.0
+var it: int
 func _specific_math():
 	#if i<self.grid_position.y/grid_size.y*3000+1500:
 		#i+=1
@@ -16,4 +17,16 @@ func _specific_math():
 		i=0
 		a+=1
 		a=fmod(a,2)
-	return a*(sin(_phase * TAU)+sin(_phase * TAU*1.5))
+	if _aprev==a and (it==0 or it==50):
+		return a*(sin(_phase * TAU)+sin(_phase * TAU*1.5))
+	else:
+		if _aprev==0.0:
+			it+=1
+			if it==20:
+				_aprev=1.0
+			return it/20.0*(sin(_phase * TAU)+sin(_phase * TAU*1.5))
+		else:
+			it-=1
+			if it==0:
+				_aprev=0.0
+			return (20.0-it)/20.0*(sin(_phase * TAU)+sin(_phase * TAU*1.5))
--- animals/qudratoppator/qudratoppator.gd
+++ animals/qudratoppator/qudratoppator.gd
@@ -5,4 +5,5 @@ extends Chorister
 func _specific_math():
 	#AudioServer.get_bus_effect(AudioServer.get_bus_index("Qudratoppator"),2).set_cutoff((self.grid_position.y/self.grid_size.y*10000+400))
 	AudioServer.get_bus_effect(AudioServer.get_bus_index("Qudratoppator"),2).set_cutoff(((EFFECT_MAX_GRID_SIZE - self.grid_position.y)/EFFECT_MAX_GRID_SIZE*10000+400))
+	AudioServer.get_bus_effect(AudioServer.get_bus_index("Qudratoppator"),5).set_wet(((EFFECT_MAX_GRID_SIZE-(EFFECT_MAX_GRID_SIZE - self.grid_position.y))/EFFECT_MAX_GRID_SIZE+0.0))
 	return fmod(_phase*TAU,3)-fmod(_phase*TAU,12)#+sin(_phase*TAU)

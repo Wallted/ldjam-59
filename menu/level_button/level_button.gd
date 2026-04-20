7c4b53c34b4a9265749478c91981d9474fca0056
Grzegorz Choinski <grzegorzchoinski97@gmail.com>
2026-04-21 01:45:07 +0200

    add new animal to level button (#82)
    Signed-off-by: Grzegorz Choiński <grzegorzchoinski97@gmail.com>

--- menu/level_button/level_button.gd
+++ menu/level_button/level_button.gd
@@ -2,7 +2,7 @@ class_name LevelButton
 extends Button
 
 @onready var completion_mark: TextureRect = $CompletionMark
-
+@onready var animated_sprite: AnimatedSprite2D = $Control/AnimatedSprite2D
 var level_idx: int = 0
 var is_completed: bool = false
 
@@ -10,3 +10,10 @@ var is_completed: bool = false
 func mark_complete():
 	self.completion_mark.show()
 	self.is_completed = true
+
+func mark_animal(idx):
+	animated_sprite.show()
+	var res_list = Species.ChoristerResMapPreload[idx]
+	animated_sprite.sprite_frames.add_frame('idle', res_list[0])
+	animated_sprite.sprite_frames.add_frame('idle', res_list[1])
+	animated_sprite.play('idle')

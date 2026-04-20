ba79e495be232a1f4e6bb736295ac17635d96a4f
adasium <adasium@proton.me>
2026-04-20 02:46:26 +0200

    show ufo on solve and lit a led

--- game.gd
+++ game.gd
@@ -7,6 +7,8 @@ extends Control
 @onready var current_fractal: Fractal = $CurrentFractal
 @onready var target_fractal: Fractal = $TargetFractal
 @onready var background = $Background
+const UFO = preload("uid://cmtktioq7jdad")
+@onready var led: Led = $Led
 
 var level: Level
 signal go_to_menu()
@@ -15,6 +17,7 @@ var rng = RandomNumberGenerator.new()
 
 func _ready():
 	back_button.pressed.connect(_exit_level)
+	led.reset()
 
 func load_new_level(level_data: LevelData) -> void:
 	level = Level.new(level_data)
@@ -24,6 +27,9 @@ func load_new_level(level_data: LevelData) -> void:
 	current_fractal.start(level)
 	target_fractal.start(level)
 	zoo_space.restart()
+	led.reset()
+	check_win_condition()
+	
 
 	for x in 8:
 		for y in 5:
@@ -40,21 +46,33 @@ func _on_play_button(is_pressed: bool, is_player: bool):
 	if is_pressed:
 		audio.who_sings = Audio.WhoSings.Player if is_player else Audio.WhoSings.Target
 	if is_pressed and is_player:
-		var is_solved = check_win_condition()
-		if is_solved:
-			print('glorp')
-		else:
-			print("glorpn't")
+		check_win_condition()
+		
+			
+func ufo_cleanup():
+	var ufo = UFO.instantiate()
+	var screen = get_viewport_rect()
+	ufo.position = screen.size + Vector2(50.0, -screen.size.y/2)
+	ufo.fly_at(Vector2(-50.0, -50.0))
+	add_child(ufo)
 
 func check_win_condition():
-	var leeway = zoo_space.tilemap.tile_set.tile_size;
 	var is_solved = true;
 	var player_choir = zoo_space.level.player_choir;
 	var target_choir = zoo_space.level.target_choir;
 	for i in min(len(player_choir), len(target_choir)):
 		var player_chorist = player_choir[i];
 		var target_chorist = target_choir[i];
-		var diff = abs(player_chorist.grid_position - target_chorist.grid_position);
-		if diff.x > leeway.x or diff.y > leeway.y:
+		if (
+			int(player_chorist.grid_position.x) != int(target_chorist.grid_position.x)
+			or int(player_chorist.grid_position.y) != int(target_chorist.grid_position.y)
+		):
 			is_solved = false;
+	if is_solved:
+		print('glorp')
+		led.enable()
+		ufo_cleanup()
+	else:
+		print("glorpn't")
+		led.disable()
 	return is_solved;

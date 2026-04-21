4bfa014bd3c89995c06c635e27d305d12d115c85
adasium <adasium@proton.me>
2026-04-21 02:49:11 +0200

    adasium/ufo cruise control (#85)
    * add ufo path
    
    * better ufo cruise control
    
    * update magic variables
    
    * fix ufo not making reappearances after first show, next is bigfoot to fix

--- ufo.gd
+++ ufo.gd
@@ -1,26 +1,2 @@
 class_name UFO
 extends AnimatedSprite2D
-
-var destination: Vector2 = Vector2.ZERO
-
-
-var SPEED: float = 200.0
-
-
-func _process(delta):
-	var distance_to_destination
-	var distance_to_move
-	if position != destination:
-		self.play()
-		distance_to_destination = position.distance_to(destination)
-		distance_to_move = SPEED * delta
-		if abs(distance_to_destination) < abs(distance_to_move):
-			distance_to_move = distance_to_destination
-		position += position.direction_to(destination) * distance_to_move
-	else:
-		self.hide()
-
-
-func fly_at(_destination: Vector2):
-	self.destination = _destination
-	self.show()
